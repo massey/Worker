c4d704ae2ea8055dc26e9d889215ef80a49bea41
Matt Andrews <mattandrews@massey.com.au>
2015-07-27 09:56:48 +1000

    Initial Commit

---
+++ Worker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Worker
+//
+//  Created by Matt Andrews on 27/07/2015.
+//  Copyright (c) 2015 Massey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Worker/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Worker
+//
+//  Created by Matt Andrews on 27/07/2015.
+//  Copyright (c) 2015 Massey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
